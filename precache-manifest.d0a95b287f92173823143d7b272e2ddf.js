109da1d39f9a0b05e89f73a57a04324c61fa5f65
Sathish Velayudam <sathish.velayudam@gmail.com>
2020-06-20 21:36:44 -0400

    Updates

---
+++ precache-manifest.d0a95b287f92173823143d7b272e2ddf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1235890712628e4dc20f1a66ad30513f",
+    "url": "/react-basics-api/index.html"
+  },
+  {
+    "revision": "5b2910e4aeb328f28c99",
+    "url": "/react-basics-api/static/js/2.2036ffcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basics-api/static/js/2.2036ffcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee0609e8f4c15eb0b1f8",
+    "url": "/react-basics-api/static/js/main.4642ac53.chunk.js"
+  },
+  {
+    "revision": "a542e5cdef9ef89080a4",
+    "url": "/react-basics-api/static/js/runtime-main.c770ad8c.js"
+  }
+]);
